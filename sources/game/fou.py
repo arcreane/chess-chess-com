8cb7e7553ea85c618ad7820e68ca11e0faab1259
vzqq4qmqkh <vzqq4qmqkh@privaterelay.appleid.com>
2026-05-22 08:30:02 +0200

    Ajout logique de déplacement fou et tour

--- sources/game/fou.py
+++ sources/game/fou.py
@@ -1,21 +1,48 @@
-def fou_p(plateau , position):
-    x , y = position
-    coups = []
-    dir = [(-1 , -1) , (-1 , 1) , (1 , -1) , (1 , 1)]
+from piece import Piece
 
-    for dx , dy in dir:
-        nx , ny = x + dx, y + dy
-        while 0 <= nx < 8 and 0 <= ny < 8:
-            if plateau[nx][ny] == '.':
-                coups.append((nx , ny))
-            elif plateau[nx][ny] == 'N':
-                coups.append((nx,ny))
-            else:
-                break
-            nx += dx
-            ny += dy
+class Bishop(Piece):
 
-    return coups
+    def isValidMove(self, newPosition, board):
+        current_col = ord(self.position.column)
+        current_row = self.position.row
+        new_col = ord(newPosition.column)
+        new_row = newPosition.row
+
+        col_diff = new_col - current_col
+        row_diff = new_row - current_row
+
+        if coll_diff == 0 and row_diff == 0:
+            return False
+
+        is_diagonal = abs(coll_diff) == abs(row_diff)
+
+        if not is_diagonal:
+            return False
+
+        step_col = 1 if col_diff > 0 else -1
+        step_row = 1 if row_diff > 0 else -1
+
+        check_col = current_col + step_col
+        check_row = current_row + step_row
+
+        while check_col != new_col or check_row != new_row:
+            intermediate_position = type(self.position)(chr(check_col), check_row)
+
+            if board.getPiece(intermediate_position) is not None:
+                return False
+
+            check_col += step_col
+            check_row += step_row
+
+        target_piece = board.getPiece(newPosition)
+
+        if self.is_same_color(target_piece):
+            return False
+
+        return True
+
+    def __str__(self):
+        return "B"
 
 
 
